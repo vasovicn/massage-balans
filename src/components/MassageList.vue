db1b8b26c8a20a230a1ead83b12f618287436af0
Nikola Vasovic <vasovicnikola97@gmail.com>
2022-10-03 09:29:21 +0200

    wip (massage-balans) changing ui and logic for massage details

--- src/components/MassageList.vue
+++ src/components/MassageList.vue
@@ -1,8 +1,11 @@
 <template>
-  <h1 style="text-align:center">Masaze</h1>
-  <div class="massages">
-    <MassageCard v-for="massage in massages" :key="massage.id" :massage="massage"/>
-  </div>
+  <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+  </head>
+      <div class="band">
+        <MassageCard v-for="massage in massages" :key="massage.id" :massage="massage" />
+      </div>
+  
 </template>
 
 <script>
@@ -12,12 +15,12 @@ export default {
   name: 'MassageList',
   components: {
     MassageCard
-},
+  },
   created() {
     this.$store.dispatch('fetchMassages')
-    .catch(error => {
-      console.log(error)
-    })
+      .catch(error => {
+        console.log(error)
+      })
   },
   computed: {
     massages() {
@@ -28,9 +31,26 @@ export default {
 </script>
 
 <style scoped>
-.massages {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  .band {
+  /* width: 90%; */
+  /* max-width: 1240px; */
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: auto;
+  gap: 10px;
+  padding:10px;
+}
+
+@media (min-width: 30em) {
+  .band {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+@media (min-width: 60em) {
+  .band {
+    grid-template-columns: repeat(4, 1fr);
+  }
 }
 </style>
--- src/components/MasseurCard.vue
+++ src/components/MasseurCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="masseur">
+    <div class="card">
       <img src="@/assets/logo.png" style="max-width: 20px;max-height: 20px;"/>{{masseur.name}}
     </div>
 </template>
@@ -12,18 +12,63 @@
   }
 </script>
 <style>
-.masseur {
-  display:inline-block;
-  flex-direction: column;
-  align-items: center;
-  border:1px solid;
-  min-width: 300px;
-  max-height: 200px;
-  padding:10px;
-}
 .event-link {
   color: #2c3e50;
   text-decoration: none;
+}
+.card {
+  background: white;
+  text-decoration: none;
+  color: #444;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+  position: relative;
+  top: 0;
+  transition: all 0.1s ease-in;
+}
+
+.card:hover {
+  top: -2px;
+  box-shadow: 0 4px 5px rgba(0, 0, 0, 0.2);
+}
+
+.card article {
+  padding: 20px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.card h1 {
+  font-size: 20px;
+  margin: 0;
+  color: #333;
+}
+
+.card p {
+  flex: 1;
+  line-height: 1.4;
+}
+
+.card span {
+  font-size: 12px;
+  font-weight: bold;
+  color: #999;
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+  margin: 2em 0 0 0;
+}
+
+.card .thumb {
+  padding-bottom: 60%;
+  background-size: cover;
+  background-position: center center;
+}
+.card:hover {
+  cursor: pointer;
 }
 </style>
 
