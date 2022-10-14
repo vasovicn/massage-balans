79428740ee7bacfb11d12179e36248afa6aafd16
nikoleta-vukajlovic <ninavukajlovic97@gmail.com>
2022-10-14 23:36:47 +0200

    verify password

--- server/user/views.py
+++ server/user/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
+from rest_framework.authtoken.models import Token
+
 
 def login(request):
     if request.method == "POST":
@@ -20,3 +22,14 @@ def login(request):
 def home(request):
     users = User.objects.all()
     return render(request, 'index.html', {'users': users})
+
+def verifyPassword(requsts):
+    token = request.body.token
+    oldPassword = request.body.odlPassword
+
+    user = User.objects.get(key=token)
+    user = auth.authenticate(username=user.username, password=oldPassword)
+
+    if user:
+        return HttpResponse(status=200)
+    return HttpResponse(status=404)
